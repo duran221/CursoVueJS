b947a8826fe46c0eec88073c369f1cf6cc406ec4
duran221 <cristianfernandoduran@gmail.com>
2020-11-26 16:34:28 -0500

    Subidos tutoriales de enrutamiento y Creación App Tareas

--- 2. Crear y Usar Proyecto/mifirstapp/src/App.vue
+++ 2. Crear y Usar Proyecto/mifirstapp/src/App.vue
@@ -1,14 +1,53 @@
 <template>
-<div id="app">
+
+<!--IMPORTANTE!!! Este div va a ser reemplazado por el div que listamos más abajo, para ver su
+    Funcionamiento, favor comentar el div 'app' y renombrar este en su defecto:
+-->
+
+
+<!--<div id="app">
 
     <h1>Hola mundo</h1>
 
     <!--La etiqueta está señalando que estamos usando este componente aquí-->
-    <Test message="Vengo de afuera"></Test>
+<!-- <Test message="Vengo de afuera"></Test>
 
     <!--Voy a usar otro componente, veámos cómo se comporta:-->
-    <User/>
+<!--    <User/>
 </div>
+-->
+
+<div id="app">
+    <!--Una simple barra de navegación que se mostrará por toda la aplicación:-->
+    <nav>
+        <ul>
+            <li>
+                <!--Me permite crear enlaces y enrutarlos hacia nuestros componentes, para que se muestre
+                    Uno u otro-->
+                <router-link to="/test">
+                    Test
+                </router-link>
+            </li>
+
+            <li>
+                <router-link to="/">
+                    User
+                </router-link>
+            </li>
+
+        </ul>
+    </nav>
+
+    <!--Aquí visualizo un componente en específico, si por ejemplo, cargo la sección de usuarios, esa
+        Se mostrará aquí:
+    -->
+    <router-view>
+
+    </router-view>
+
+</div>
+
+
 </template>
 
 <script>
--- 2. Crear y Usar Proyecto/mifirstapp/src/main.js
+++ 2. Crear y Usar Proyecto/mifirstapp/src/main.js
@@ -8,12 +8,52 @@ import VueResource from 'vue-resource';
 //Indicandole a vue, que vamos a usar dicho módulo:
 Vue.use(VueResource);
 
+//Importando el módulo vue-router que nos permitirá enrutar páginas dentro de la aplicación:
+import VueRouter from 'vue-router';
+//Usando el módulo que importamos anteriormente:
+Vue.use(VueRouter)
+
+//Importando los componentes que vamos a enrutar en la aplicación:
+import Test from './components/Test';
+import User from './components/User';
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+const router = new VueRouter({
+    //MODE: Permite que el navegador conserve la dirección url siguiendo el patrón indicado abajo:
+    mode: 'history',
+    //Constante que me indica la raíz del directorio:
+    base: __dirname,
+    //ROUTES: Lista de las rutas que seguirá la URL contenida dentro del proyecto VueJS:
+    routes: [{
+            //Esta ruta me indica qué componente se cargará al iniciar la aplicación:
+            path: '/',
+            //Nombre del componente:
+            component: User
+        },
+        {
+            path: '/test',
+            component: Test
+        }
+    ]
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-    el: '#app',
+    //Con esta propiedad indico que voy a enrutar los componentes que hemos creado:
+    router,
+
+    //Como vamos a enrutar diferentes pestañas, el siguiente atributo no se usará:
+    // el: '#app',
+
+    //Aquí indicamos el componente principal que va a contener a los demás:
     components: { App },
     template: '<App/>'
-})
+
+    //Indicamos más abajo cuál será el template en el cual vamos a montar este recurso:    
+}).$mount('#app');
